1512f9cc0ce0a1c607223347327567ec94a74a89
Viktor Rura <ruravictor@gmail.com>
2023-05-28 21:20:26 +0100

    deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn habit_builder.wsgi
---
+++ ritual/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RitualConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ritual'
